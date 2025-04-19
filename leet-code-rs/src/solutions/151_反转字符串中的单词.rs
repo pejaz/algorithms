e942ad4b61c78b0ef661167cd88ebd78cab0c157
pejaz <candane@126.com>
2025-04-20 02:39:36 +0800

    wip: 151

---
+++ leet-code-rs/src/solutions/151_反转字符串中的单词.rs
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=151 lang=rust
+ *
+ * [151] 反转字符串中的单词
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn reverse_words(s: String) -> String {
+        
+    }
+}
+// @lc code=end
+
+
+#[allow(unused)]
+struct Solution;
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_reverse_words() {
+        //assert_eq!(Solution::reverse_words(vec![]),[]);
+        assert!(true)
+    }
+}
