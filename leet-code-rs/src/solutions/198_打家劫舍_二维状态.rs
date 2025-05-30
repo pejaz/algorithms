59d78790802b8ea7cf0100b1223efcc2814d790b
pejaz <candane@126.com>
2025-05-30 20:49:07 +0800

    feat: 🎉打家劫舍总结

--- leet-code-rs/src/solutions/198_打家劫舍_二维状态.rs
+++ leet-code-rs/src/solutions/198_打家劫舍_二维状态.rs
@@ -12,7 +12,7 @@ impl Solution {
      * 解题思路： 2，1，1，2
      *  1. DP 数组的含义： dp[i] 代表到 下标 i 的位置所能投的最大金额,(不偷最大值，偷最大值)
      *  2. 递推公式：如果
-     *      dp[i] 不偷，所以维持 i-1 状态 dp[i] = max(dp[i-1](不偷) ,dp[i-1] (偷));
+     *      dp[i] 不偷，则考虑 i-1 最大值就（即 dp[i-1]状态），i-1 不一定偷，可能偷 i-2： dp[i] = max(dp[i-1](不偷) ,dp[i-1] (偷));
      *      dp[i] 偷，dp[i] = dp[i-1](不偷) + money[i]
      *      最终 dp[i] 取值这两种的最大值。
      *  3. 初始化，依赖i-1 状态 dp[0] 和 dp[1]
@@ -27,12 +27,13 @@ impl Solution {
         for i in 1..nums.len() {
             // 避免 i-2 下标为负数
             if i == 1 {
-                // 0 偷了，则 1 不能偷，0 没偷，则 1 偷
-                dp[i] = (dp[0].0.max(dp[0].1), nums[i]);
+                // 没偷 1，则偷 0；偷 1
+                dp[i] = (dp[0].1, nums[i]);
                 continue;
             }
 
-            dp[i] = (dp[i - 1].0.max(dp[i - 1].1), dp[i - 1].0 + nums[i]);
+            // 没偷 i,则i-1 不一定偷; 偷 i，则不偷 n-1
+            dp[i] = (dp[i - 1].1.max(dp[i-1].0), dp[i - 1].0 + nums[i]);
         }
 
         return dp[nums.len() - 1].0.max(dp[nums.len() - 1].1);
@@ -50,7 +51,12 @@ mod test {
 
     #[test]
     fn test_rob() {
-        //assert_eq!(Solution::rob(vec![]),[]);
+        
+        assert_eq!(Solution::rob(vec![1]), 1);
+        assert_eq!(Solution::rob(vec![1, 2]), 2);
+        assert_eq!(Solution::rob(vec![2, 3, 2]), 4);
+        assert_eq!(Solution::rob(vec![1, 2, 3, 1]), 4);
+        assert_eq!(Solution::rob(vec![2, 1, 2, 5]), 7);
         assert!(true)
     }
 }
